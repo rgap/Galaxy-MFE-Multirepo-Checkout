89d5b40168f24575951320eb61c4e84a18af00b4
rgap <r.guzmanap@gmail.com>
2025-06-07 11:41:45 -0500

    fix: Changed Checkout page
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   src/components/CartWidgetWrapper.js
    	modified:   src/pages/CheckoutPage.js

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import CartWidgetWrapper from '../components/CartWidgetWrapper';
 import CheckoutWidget from '../components/CheckoutWidget';
 
 const CheckoutPage = () => {
   return (
     <div style={{
-      maxWidth: '800px',
+      maxWidth: '1200px',
       margin: '2rem auto',
       padding: '0 1rem'
     }}>
       <h1 style={{ marginBottom: '2rem' }}>Checkout</h1>
-      <CheckoutWidget />
+      
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gap: '2rem',
+        alignItems: 'start'
+      }}>
+        <div>
+          <h2 style={{ marginBottom: '1rem' }}>Shopping Cart</h2>
+          <CartWidgetWrapper />
+        </div>
+
+        <div>
+          <h2 style={{ marginBottom: '1rem' }}>Order Summary</h2>
+          <CheckoutWidget />
+        </div>
+      </div>
     </div>
   );
 };
